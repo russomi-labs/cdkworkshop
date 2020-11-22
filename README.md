77a6b442899411a0ce3f220dae4cc97ec1c21660
Michael Russo <russo.mail@gmail.com>
2020-11-22 15:12:30 -0500

    Completed python cdkworkshop

--- README.md
+++ README.md
@@ -14,39 +14,39 @@ manually once the init process completes.
 
 To manually create a virtualenv on MacOS and Linux:
 
-```
+```bash
 $ python3 -m venv .venv
 ```
 
 After the init process completes and the virtualenv is created, you can use the following
 step to activate your virtualenv.
 
-```
+```bash
 $ source .venv/bin/activate
 ```
 
 If you are a Windows platform, you would activate the virtualenv like this:
 
-```
+```bash
 % .venv\Scripts\activate.bat
 ```
 
 Once the virtualenv is activated, you can install the required dependencies.
 
-```
+```bash
 $ pip install -r requirements.txt
 ```
 
 At this point you can now synthesize the CloudFormation template for this code.
 
-```
+```bash
 $ cdk synth
 ```
 
 You can now begin exploring the source code, contained in the hello directory.
 There is also a very trivial test included that can be run like this:
 
-```
+```bash
 $ pytest
 ```
 
--- cdkworkshop/cdkworkshop_stack.py
+++ cdkworkshop/cdkworkshop_stack.py
@@ -1,24 +1,39 @@
 from aws_cdk import (
-    aws_iam as iam,
-    aws_sqs as sqs,
-    aws_sns as sns,
-    aws_sns_subscriptions as subs,
-    core
+    core,
+    aws_lambda as _lambda,
+    aws_apigateway as apigw,
 )
+
+from cdk_dynamo_table_viewer import TableViewer
+from hitcounter import HitCounter
 
 
 class CdkworkshopStack(core.Stack):
 
-    def __init__(self, scope: core.Construct, construct_id: str, **kwargs) -> None:
-        super().__init__(scope, construct_id, **kwargs)
+    def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
 
-        queue = sqs.Queue(
-            self, "CdkworkshopQueue",
-            visibility_timeout=core.Duration.seconds(300),
+        # Defines an AWS Lambda resource
+        hello = _lambda.Function(
+            self, 'HelloHandler',
+            runtime=_lambda.Runtime.PYTHON_3_7,
+            code=_lambda.Code.asset('lambda'),
+            handler='hello.handler',
         )
 
-        topic = sns.Topic(
-            self, "CdkworkshopTopic"
+        hello_with_counter = HitCounter(
+            self, 'HelloHitCounter',
+            downstream=hello,
         )
 
-        topic.add_subscription(subs.SqsSubscription(queue))
+        apigw.LambdaRestApi(
+            self, 'Endpoint',
+            handler=hello_with_counter.handler,
+        )
+
+        TableViewer(
+            self, 'ViewHitCounter',
+            title='Hello Hits',
+            table=hello_with_counter.table,
+            sort_by='-hits',
+        )
